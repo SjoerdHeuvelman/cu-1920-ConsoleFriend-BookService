74574b7f235951aed359a2c89dde9b23c8897d9c
Joachim Francois <joachim.francois@howest.be>
2019-09-19 14:33:14 +0200

    Add BooksController

---
+++ BookService.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookService.WebAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        readonly BookRepository _bookRepository;
+
+        public BooksController(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(_bookRepository.List());
+        }
+
+        // GET: api/Books/Basic
+        [HttpGet]
+        [Route("Basic")]
+        public IActionResult GetBooksBasic()
+        {
+            return Ok(_bookRepository.ListBasic());
+        }
+    }
+}
