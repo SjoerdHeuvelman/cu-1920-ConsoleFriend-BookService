832c2bbee92a4b34b6fcf3483c4f2c45f7eaa7ad
Joachim Francois <joachim.francois@howest.be>
2019-09-19 15:44:58 +0200

    Add PublisherController

---
+++ BookService.WebAPI/Controllers/PublisherController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookService.WebAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        readonly PublisherRepository _publisherRepository;
+
+        public PublisherController(PublisherRepository publisherRepository)
+        {
+            _publisherRepository = publisherRepository;
+        }
+
+        // GET: api/Publisher
+        [HttpGet]
+        public async Task<IActionResult> GetPublishers()
+        {
+            return Ok(_publisherRepository.List());
+        }
+
+        // GET: api/Publisher/2
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPublisher(int id)
+        {
+            return Ok(_publisherRepository.GetById(id));
+        }
+    }
+}
