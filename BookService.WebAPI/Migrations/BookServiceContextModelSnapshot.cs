fdf3e752d4cd0ec9eece1110206dda7a516aaf9e
Joachim Francois <joachim.francois@howest.be>
2019-09-19 08:43:55 +0200

    Add BookBasic dto and cleanup unused references

--- BookService.WebAPI/Migrations/BookServiceContextModelSnapshot.cs
+++ BookService.WebAPI/Migrations/BookServiceContextModelSnapshot.cs
@@ -4,7 +4,6 @@ using BookService.WebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookService.WebAPI.Migrations
 {
