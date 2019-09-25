d699ec75fbf8877a83e756ba9edba1602b32f18f
Joachim Francois <joachim.francois@howest.be>
2019-09-25 21:02:44 +0200

    Add EntityBase

---
+++ BookService.WebAPI/Models/EntityBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Models
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+
+        private DateTime? createdDateTime;
+
+        public DateTime? CreatedDateTime
+        {
+            get
+            {
+                return createdDateTime ?? DateTime.Now;
+            }
+            set
+            {               
+                if (value != null)
+                {
+                    createdDateTime = value;
+                }
+                else
+                {
+                    createdDateTime = DateTime.Now;
+                }
+            }
+        }
+
+    }
+}
