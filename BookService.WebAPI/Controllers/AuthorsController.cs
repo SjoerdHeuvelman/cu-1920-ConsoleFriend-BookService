7b48533c2dcc8d1233dba63999a0e5215930fba6
Joachim Francois <joachim.francois@howest.be>
2019-10-01 14:52:56 +0200

    Update Authors and PublisherController with ControllerCrudBase implementation

--- BookService.WebAPI/Controllers/AuthorsController.cs
+++ BookService.WebAPI/Controllers/AuthorsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookService.WebAPI.Models;
 using BookService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,20 +11,10 @@ namespace BookService.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthorsController : ControllerBase
+    public class AuthorsController : ControllerCrudBase<Author, AuthorRepository>
     {
-        readonly AuthorRepository _authorRepository;
-
-        public AuthorsController(AuthorRepository authorRepository)
-        {
-            _authorRepository = authorRepository;
-        }
-
-        // GET: api/Authors
-        [HttpGet]
-        public async Task<IActionResult> GetAuthors()
-        {
-            return Ok(await _authorRepository.ListAll());
+        public AuthorsController(AuthorRepository authorRepository) : base(authorRepository)
+        {            
         }
 
         // GET: api/Authors/Basic
@@ -31,14 +22,7 @@ namespace BookService.WebAPI.Controllers
         [Route("Basic")]
         public async Task<IActionResult> GetAuthorBasic()
         {
-            return Ok(await _authorRepository.ListBasic());
-        }
-
-        // GET: api/Authors/2
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetAuthor(int id)
-        {
-            return Ok(await _authorRepository.GetById(id));
-        }
+            return Ok(await repository.ListBasic());
+        }        
     }
 }
--- BookService.WebAPI/Controllers/PublisherController.cs
+++ BookService.WebAPI/Controllers/PublisherController.cs
@@ -11,82 +11,12 @@ namespace BookService.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PublisherController : ControllerBase
-    {
-        readonly PublisherRepository _publisherRepository;
+    public class PublisherController : ControllerCrudBase<Publisher, PublisherRepository>
+    {    
 
-        public PublisherController(PublisherRepository publisherRepository)
-        {
-            _publisherRepository = publisherRepository;
-        }
-
-        // GET: api/Publisher
-        [HttpGet]
-        public async Task<IActionResult> GetPublishers()
-        {
-            return Ok(await _publisherRepository.ListAll());
-        }
-
-        // GET: api/Publisher/2
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetPublisher(int id)
-        {
-            return Ok(await _publisherRepository.GetById(id));
-        }
-
-        //PUT: api/Publisher/2
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPublisher([FromRoute]int id, [FromBody] Publisher publisher)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if(id != publisher.Id)
-            {
-                return BadRequest();
-            }
-
-            var updatedPublisher = await _publisherRepository.Update(publisher);
-            if(updatedPublisher == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(updatedPublisher); 
-        }
-
-        //POST: api/Publisher
-        [HttpPost]
-        public async Task<IActionResult> PostPublisher([FromBody] Publisher publisher)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            await _publisherRepository.Add(publisher);
-
-            return CreatedAtAction(nameof(GetPublisher), new { id = publisher.Id }, publisher);
-        }
-
-        //DELETE: api/Publisher/4
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePublisher([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var publisher = await _publisherRepository.Delete(id);
-            if(publisher == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(publisher);            
-        }
+        public PublisherController(PublisherRepository publisherRepository): base(publisherRepository)
+        {            
+        }       
+        
     }
 }
