4ad644f147c03d02c0a46a001bb93e2fe75740a0
Joachim Francois <joachim.francois@howest.be>
2019-10-03 15:41:28 +0200

    Add Rating- and ReaderRepository

---
+++ BookService.WebAPI/Repositories/RatingRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookService.WebAPI.Data;
+using BookService.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class RatingRepository : RepositoryBase<Rating>
+    {
+        public RatingRepository(BookServiceContext bookServiceContext) : base(bookServiceContext)
+        {
+        }
+
+        public async Task<List<Rating>> GetAllInclusive()
+        {
+            return await GetAll()
+                .Include(r => r.Book)
+                .Include(r => r.Reader)
+                .ToListAsync();
+        }
+    }
+}
---
+++ BookService.WebAPI/Repositories/ReaderRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookService.WebAPI.Data;
+using BookService.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class ReaderRepository : RepositoryBase<Reader>
+    {
+        public ReaderRepository(BookServiceContext bookServiceContext) : base(bookServiceContext)
+        {
+        }
+    }
+}
