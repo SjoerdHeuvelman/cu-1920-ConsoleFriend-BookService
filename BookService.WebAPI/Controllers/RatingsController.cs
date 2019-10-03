79c9cdc3daa80dab32db46c378077f592a6a8b0c
Joachim Francois <joachim.francois@howest.be>
2019-10-03 15:42:04 +0200

    Add Ratings- and ReadersController

---
+++ BookService.WebAPI/Controllers/RatingsController.cs
@@ -0,0 +1,40 @@
+﻿using BookService.WebAPI.Models;
+using BookService.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerCrudBase<Rating, RatingRepository>
+    {
+        public RatingsController(RatingRepository ratingRepository) : base(ratingRepository)
+        {
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> Get()
+        {
+            return Ok(await repository.GetAllInclusive());
+        }
+
+        /*Om een selfreferencing loop te vermijden OPTIE 1
+        // GET: api/Ratings
+        [HttpGet]
+        public override async Task<IActionResult> Get()
+        {
+            
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            return Ok(JsonConvert.SerializeObject(await repository.GetAllInclusive(),settings));
+        }
+        */
+
+    }
+}
---
+++ BookService.WebAPI/Controllers/ReadersController.cs
@@ -0,0 +1,19 @@
+﻿using BookService.WebAPI.Models;
+using BookService.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadersController : ControllerCrudBase<Reader, ReaderRepository>
+    {
+        public ReadersController(ReaderRepository readerRepository) : base(readerRepository)
+        {
+        }
+    }
+}
