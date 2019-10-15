063ee76bb3b3856ec4a87c52bf50f84a4f6032d2
Sjoerd Heuvelman <Sjoerd_Heuvelman@hotmail.com>
2019-10-15 16:16:26 +0200

    Add Detail Page for Books

--- BookService.MVC/Controllers/BooksController.cs
+++ BookService.MVC/Controllers/BooksController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BookService.Lib.DTO;
+using BookService.Lib.Models;
+using BookService.MVC.Models.Books;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -16,6 +18,22 @@ namespace BookService.MVC.Controllers
         {
             string bookUri = $"{baseUri}/basic";
             return View(GetApiResult<List<BookBasicDto>>(bookUri));
+        }
+
+        public IActionResult Detail(int id)
+        {
+            string geekJokesUri = "https://geek-jokes.sameerkumar.website/api"; 
+            string ipsumUri = "https://baconipsum.com/api/?type=meat-and-filler&paras=2&format=text"; 
+            string bookUri = $"{baseUri}/detail/{id}";
+
+            var viewModel = new BookDetailViewModel
+            {
+                BookDetail = GetApiResult<BookDetail>(bookUri),
+                AuthorJoke = GetApiResult<string>(geekJokesUri),
+                BookSummary = new HttpClient().GetStringAsync(ipsumUri).Result
+            };
+
+            return View(viewModel);
         }
 
         private T GetApiResult<T>(string uri)
---
+++ BookService.MVC/Models/Books/BookDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BookService.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.MVC.Models.Books
+{
+    public class BookDetailViewModel
+    {
+        public BookDetail BookDetail { get; set; }
+        public string AuthorJoke { get; set; }
+        public string BookSummary { get; set; }
+    }
+}
