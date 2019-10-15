a38932f0e5407ed115066228316f7f4952b0c516
Joachim Francois <joachim.francois@howest.be>
2019-10-15 08:35:27 +0200

    Add PublisherController

---
+++ BookService.MVC/Controllers/PublishersController.cs
@@ -0,0 +1,24 @@
+﻿using BookService.Lib.DTO;
+using BookService.Lib.Models;
+using BookService.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BookService.MVC.Controllers
+{
+    public class PublishersController : Controller
+    {
+        string baseUri = "https://localhost:5001/api/publisher";
+        public IActionResult Index()
+        {
+            string publisherUri = $"{baseUri}/basic";
+            return View(WebApiService.GetApiResult<List<PublisherBasicDto>>(publisherUri));
+        }
+
+        public IActionResult Detail(int id)
+        {
+            string publisherUri = $"{baseUri}/{id}";
+            return View(WebApiService.GetApiResult<Publisher>(publisherUri));
+        }
+    }
+}
