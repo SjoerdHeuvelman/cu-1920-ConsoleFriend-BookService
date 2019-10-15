d1482807feae39e5d4162aa12f39f5e1b7bb21df
Joachim Francois <joachim.francois@howest.be>
2019-10-15 15:47:37 +0200

    Update PublishersController

--- BookService.MVC/Controllers/PublishersController.cs
+++ BookService.MVC/Controllers/PublishersController.cs
@@ -3,6 +3,7 @@ using BookService.Lib.Models;
 using BookService.MVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BookService.MVC.Controllers
 {
@@ -18,7 +19,44 @@ namespace BookService.MVC.Controllers
         public IActionResult Detail(int id)
         {
             string publisherUri = $"{baseUri}/{id}";
+            ViewBag.Mode = "Detail";
             return View(WebApiService.GetApiResult<Publisher>(publisherUri));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            string publisherUri = $"{baseUri}/{id}";
+            ViewBag.Mode = "Edit";
+            return View(nameof(Detail), WebApiService.GetApiResult<Publisher>(publisherUri));
+        }
+
+        public IActionResult Insert()
+        {
+            ViewBag.Mode = "Edit";
+            return View(nameof(Detail), new Publisher());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Publisher publisher)
+        {
+            if (publisher.Id != 0)
+            {
+                string uri = $"{baseUri}/{publisher.Id}";
+                publisher = await WebApiService.PutCallApi<Publisher, Publisher>(uri, publisher);
+            }
+            else
+            {
+                publisher = await WebApiService.PostCallApi<Publisher, Publisher>(baseUri, publisher);
+            }
+            ViewBag.Mode = "Detail";
+            return View(nameof(Detail), publisher);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            string uri = $"{baseUri}/{id}";
+            Publisher publisher = await WebApiService.DeleteCallApi<Publisher>(uri);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
