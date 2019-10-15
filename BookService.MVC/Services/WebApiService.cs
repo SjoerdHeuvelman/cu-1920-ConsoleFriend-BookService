75842bfd0bfce2b4d1777636dfc5736741df9d72
Joachim Francois <joachim.francois@howest.be>
2019-10-15 08:35:16 +0200

    Add WebApiService.cs

---
+++ BookService.MVC/Services/WebApiService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BookService.MVC.Services
+{
+    public class WebApiService
+    {
+        public static T GetApiResult<T>(string uri)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<string> response = httpClient.GetStringAsync(uri);
+                return Task.Factory.StartNew(
+                    () => JsonConvert.DeserializeObject<T>(response.Result)
+                ).Result;
+            }
+        }
+    }
+}
