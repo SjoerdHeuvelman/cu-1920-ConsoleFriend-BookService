7bfc07084f82f93eb3a173dc3c0d387aa61ac388
Joachim Francois <joachim.francois@howest.be>
2019-10-15 08:16:19 +0200

    Update BooksController

--- BookService.MVC/Controllers/BooksController.cs
+++ BookService.MVC/Controllers/BooksController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BookService.Lib.DTO;
+using BookService.Lib.Models;
+using BookService.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -11,12 +13,25 @@ namespace BookService.MVC.Controllers
 {
     public class BooksController : Controller
     {
-        string baseUri = "https://localhost:4200/api/books";
+        string baseUri = "https://localhost:5001/api/books";
         public IActionResult Index()
         {
             string bookUri = $"{baseUri}/basic";
 
             return View(GetApiResult<List<BookBasicDto>>(bookUri));
+        }
+
+        public IActionResult Detail(int id)
+        {
+            string geekJokesUri = "https://geek-jokes.sameerkumar.website/api";
+            string ipsumUri = "https://baconipsum.com/api/?type=meat-andfiller&paras=2&format=text";
+            string bookUri = $"{baseUri}/detail/{id}";
+            return View(new BooksDetailViewModel
+            {
+                BookDetail = GetApiResult<BookDetail>(bookUri),
+                AuthorJoke = GetApiResult<string>(geekJokesUri),
+                BookSummary = new HttpClient().GetStringAsync(ipsumUri).Result 
+            });
         }
 
         private T GetApiResult<T>(string uri)
